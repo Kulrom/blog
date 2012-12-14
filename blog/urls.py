96b3501b792ef06d2cc5b9a283fad2aa9a8af722
Jonny Ford <jonny.m.ford@gmail.com>
2012-12-14 15:52:15 +0000

    Fixed bug in urls.py where not all slugs were recognised

--- blog/urls.py
+++ blog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'blog.views.home', name='home'),
     # url(r'^blog/', include('blog.foo.urls')),
 
-    url(r'^post/(\w+)/$', 'post.views.render_post'),
+    url(r'^post/(.+)/$', 'post.views.render_post'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'post.views.list_posts'),
 )
