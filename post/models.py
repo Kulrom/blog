00ef832f4f0df6217b34a4e037c9653f66955724
Jonny Ford <jonny@jonny-lubuntu.(none)>
2012-12-21 20:08:49 +0000

    Lists posts on homepage in reverse date/time order and limits to the latest 10 posts

--- post/models.py
+++ post/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 class Post(models.Model):
-	title = models.CharField(max_length=255)
-	post_date = models.DateTimeField(auto_now_add=True)
-	edit_date = models.DateTimeField(auto_now=True)
-	markdown = models.TextField()
-	public = models.BooleanField()
-	slug = models.SlugField(unique=True, default='abc')
+    title = models.CharField(max_length=255)
+    post_date = models.DateTimeField(auto_now_add=True)
+    edit_date = models.DateTimeField(auto_now=True)
+    markdown = models.TextField()
+    public = models.BooleanField()
+    slug = models.SlugField(unique=True, default='abc')
 
-	def __unicode__(self):
-		return self.title;
+    class Meta:
+        ordering = ['-post_date']
+
+    def __unicode__(self):
+        return self.title
--- post/views.py
+++ post/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from post.models import Post
 
 def render_post(request, post_id):
-	p = get_object_or_404(Post, slug=post_id)
-	return render(request, 'view_post.html', {'post': p})
+    p = get_object_or_404(Post, slug=post_id)
+    return render(request, 'view_post.html', {'post': p})
 
 def list_posts(request):
-	posts = get_list_or_404(Post, public=True)
-	return render(request, 'list_posts.html', {'posts': posts})
+    posts = list(Post.objects.filter(public=True)[:10])
+    if not posts:
+        raise Http404
+    return render(request, 'list_posts.html', {'posts': posts})
