a707fcd722926bed2c7c0ac46bda5fce7e3d2c78
Jonny Ford <jonny@jonny-lubuntu.(none)>
2012-12-21 20:23:10 +0000

    Navigate to next and previous posts (if they exist) with guillemet (new WOTD) markers

--- post/views.py
+++ post/views.py
@@ -1,10 +1,19 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
 from post.models import Post
 
 def render_post(request, post_id):
     p = get_object_or_404(Post, slug=post_id)
-    return render(request, 'view_post.html', {'post': p})
+    try:
+        next_post = Post.objects.get(pk=(p.pk + 1))
+    except ObjectDoesNotExist:
+        next_post = None
+    try:
+        previous_post = Post.objects.get(pk=(p.pk - 1))
+    except ObjectDoesNotExist:
+        previous_post = None
+    return render(request, 'view_post.html', {'post': p, 'next_post': next_post, 'previous_post': previous_post})
 
 def list_posts(request):
     posts = list(Post.objects.filter(public=True)[:10])
